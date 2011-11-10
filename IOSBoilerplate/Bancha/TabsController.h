66555f5529d799b78741a7ed57188e99d64da11a
Nicholas Valbusa <siogiv@Macbook.local>
2011-11-10 16:37:47 +0100

    tabs controller added

---
+++ IOSBoilerplate/Bancha/TabsController.h
@@ -0,0 +1,13 @@
+//
+//  TabsController.h
+//  Bancha
+//
+//  Created by Nicholas Valbusa on 10/11/11.
+//  Copyright (c) 2011 Squallstar Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabsController : UITabBarController
+
+@end
