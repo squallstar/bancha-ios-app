dfaba10fdbc38e8fa215e936bdc12f0b359f4c3e
Nicholas Valbusa <siogiv@Macbook.local>
2011-11-10 16:37:58 +0100

    contents controller added

---
+++ IOSBoilerplate/Bancha/ContentsController.h
@@ -0,0 +1,27 @@
+//
+//  ContentsController.h
+//  Bancha
+//
+//  Created by Nicholas Valbusa on 10/11/11.
+//  Copyright (c) 2011 Squallstar Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Constants.h"
+
+@interface ContentsController : UITableViewController {
+    Structure structure;
+    NSMutableArray *pages;
+    NSMutableArray *contents;
+    NSDictionary *types;
+}
+
+- (void)fillContentTypes;
+
+@property (nonatomic, assign) Structure structure;
+
+@property (nonatomic, retain) NSMutableArray *pages;
+@property (nonatomic, retain) NSMutableArray *contents;
+@property (nonatomic, retain) NSDictionary *types;
+
+@end
---
+++ IOSBoilerplate/Bancha/ContentsController.m
@@ -0,0 +1,177 @@
+//
+//  ContentsController.m
+//  Bancha
+//
+//  Created by Nicholas Valbusa on 10/11/11.
+//  Copyright (c) 2011 Squallstar Studio. All rights reserved.
+//
+
+#import "ContentsController.h"
+
+
+@implementation ContentsController
+
+@synthesize structure, pages, contents;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)fillContentTypes {
+    [pages removeAllObjects];
+    [contents removeAllObjects];
+    
+    types = [[NSUserDefaults standardUserDefaults] objectForKey:@"content_types"];
+    NSArray *keys = [types allKeys];
+    NSLog(@"%@", keys);
+    for (NSString *key in keys) {
+        NSLog(@"%@", [[types objectForKey:key] objectForKey:@"tree"]);
+        if ([[types objectForKey:key] objectForKey:@"tree"]) {
+            
+        }
+    }
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    pages = [[NSMutableArray alloc] initWithCapacity:10];
+    contents = [[NSMutableArray alloc] initWithCapacity:30];
+    
+    [self fillContentTypes];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+}
+
+@end
