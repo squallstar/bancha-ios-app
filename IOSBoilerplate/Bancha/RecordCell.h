6372115ccc24807ce02f2bbf6d1499409937299c
Nicholas Valbusa <info@squallstar.it>
2011-11-14 18:18:32 +0100

    delegate added

--- IOSBoilerplate/Bancha/RecordCell.h
+++ IOSBoilerplate/Bancha/RecordCell.h
@@ -3,25 +3,37 @@
 //  Bancha
 //
 //  Created by Nicholas Valbusa on 11/11/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Squallstar Studio. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+@protocol RecordCellDelegate 
+@optional
+-(void)cellClickedEditButton:(int)record_id;
+@end
+
 @interface RecordCell : UITableViewCell {
+	id<RecordCellDelegate> delegate;
 	CGFloat cellHeight; 
 	UILabel *title;
 	UILabel *firstLine;
 	UILabel *secondLine;
 	UIView *background;
+	UIButton *btnPublish;
+	int record_id;
 }
 
 -(void)setStage:(BOOL)stage;
+-(IBAction)edit:(id)sender;
 
+@property (nonatomic, retain) id<RecordCellDelegate> delegate;
 @property (nonatomic) CGFloat cellHeight;
 @property (nonatomic, retain) IBOutlet UILabel *title;
 @property (nonatomic, retain) IBOutlet UILabel *firstLine;
 @property (nonatomic, retain) IBOutlet UILabel *secondLine;
 @property (nonatomic, retain) IBOutlet UIView *background;
+@property (nonatomic, retain) IBOutlet UIButton *btnPublish;
+@property (nonatomic) int record_id;
 
 @end
--- IOSBoilerplate/Bancha/RecordCell.m
+++ IOSBoilerplate/Bancha/RecordCell.m
@@ -3,7 +3,7 @@
 //  Bancha
 //
 //  Created by Nicholas Valbusa on 11/11/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Squallstar Studio. All rights reserved.
 //
 
 #import "RecordCell.h"
@@ -11,14 +11,15 @@
 
 @implementation RecordCell
 
-@synthesize title, background, firstLine, secondLine, cellHeight;
+@synthesize title, background, firstLine, secondLine, cellHeight, btnPublish;
+@synthesize record_id, delegate;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-		self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 70.0);
+		self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 55.0);
     }
     return self;
 }
@@ -26,8 +27,18 @@
 -(void)setStage:(BOOL)stage {
 	if (stage) {
 		[[self background] setBackgroundColor:RGB(255, 244, 195)];
+		[btnPublish setTitle:@"Draft" forState:UIControlStateNormal];
+		[btnPublish setTitle:@"Draft" forState:UIControlStateHighlighted];
 	} else {
 		[[self background] setBackgroundColor:[UIColor clearColor]];
+		[btnPublish setTitle:@"Published" forState:UIControlStateNormal];
+		[btnPublish setTitle:@"Published" forState:UIControlStateHighlighted];
+	}
+}
+
+-(IBAction)edit:(id)sender {
+	if (delegate != nil) {
+		[delegate cellClickedEditButton:record_id];
 	}
 }
 
